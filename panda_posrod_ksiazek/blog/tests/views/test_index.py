e41c51b21fe841a20f47ca299d3b77f0f9c19845
Maciej Wrzeszcz <maciejwrzeszcz1@wp.pl>
2020-02-23 20:04:46 +0100

    Update test_index.py

--- panda_posrod_ksiazek/blog/tests/views/test_index.py
+++ panda_posrod_ksiazek/blog/tests/views/test_index.py
@@ -12,7 +12,7 @@ class TestIndex(test.TestCase):
         request = self.factory.get('/')
         request.user = AnonymousUser()
 
-        # Validing context sfff
+        # Validing context sfffff
         valid_context = {
             'is_user_authenticated': False,
             'hottests_posts': [],
